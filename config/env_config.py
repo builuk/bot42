a3c0d30935f2e00a44672dabd48546b47eb6bb8f
aabuiluk <andrii.a.builuk@gmail.com>
2024-12-03 18:03:49 +0200

    Run tests Bot Check

--- config/env_config.py
+++ config/env_config.py
@@ -4,7 +4,5 @@ import os
 config = dotenv_values("../.env")
 TOKEN = config.get('TOKEN')
 ADMIN_ID = config.get('ADMIN_ID')
-# TOKEN = '7800222511:AAHsM7pQUjnKr655TD9TuZI1kgZGQEgP4Do'
-# ADMIN_ID = "259158244"
 API_URL = f"https://api.telegram.org/bot{TOKEN}/"
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'config'))
