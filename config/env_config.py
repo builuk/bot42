6c2e13b8451ec9593c75b9ffe3e6d2097e881536
aabuiluk <andrii.a.builuk@gmail.com>
2024-12-03 22:40:27 +0200

    Run tests

--- config/env_config.py
+++ config/env_config.py
@@ -1,9 +1,13 @@
 from dotenv import dotenv_values
+from dotenv import load_dotenv
 import os
 
-config = dotenv_values("../.env")
-TOKEN = config.get('TOKEN')
-ADMIN_ID = config.get('ADMIN_ID')
+# config = dotenv_values("../.env")
+# TOKEN = config.get('TOKEN')
+# ADMIN_ID = config.get('ADMIN_ID')
+load_dotenv()
+TOKEN = os.getenv("TOKEN")
+ADMIN_ID = os.getenv("ADMIN_ID")
 # TOKEN = '7800222511:AAHsM7pQUjnKr655TD9TuZI1kgZGQEgP4Do'
 # ADMIN_ID = "259158244"
 API_URL = f"https://api.telegram.org/bot{TOKEN}/"
