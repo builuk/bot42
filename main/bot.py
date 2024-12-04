dbf9f868f5ee029a5080d042d6e887db59e589f1
aabuiluk <andrii.a.builuk@gmail.com>
2024-12-05 00:43:12 +0200

    AWS Check

--- main/bot.py
+++ main/bot.py
@@ -11,8 +11,14 @@ def main_loop(duration_minutes=5):
     start_time = datetime.now()
     end_time = start_time + timedelta(minutes=duration_minutes)
     offset = 0
+    last_reported_minute = None
     print(f"Bot started. Running for {duration_minutes} minutes...")
+
     while datetime.now() < end_time:
+        remaining_minutes = (end_time - datetime.now()).seconds // 60
+        if last_reported_minute != remaining_minutes:
+            print(f"Time remaining: {remaining_minutes} minutes")
+            last_reported_minute = remaining_minutes
         response = requests.get(API_URL + f"getUpdates?offset={offset}")
         updates = response.json().get('result', [])
         for update in updates:
@@ -23,7 +29,6 @@ def main_loop(duration_minutes=5):
                 role = check_user_role(user_id)
                 handler = CommandHandlerFactory().get_handler(role)
                 handler.handle(message)
-        print(f"Time remaining: {(end_time - datetime.now()).seconds // 60} minutes")
 
     print("Bot finished running.")
 
--- main/bot_with_db.py
+++ main/bot_with_db.py
@@ -39,8 +39,9 @@ def main_loop(duration_minutes=5):
     start_time = datetime.now()
     end_time = start_time + timedelta(minutes=duration_minutes)
     offset = 0
-
+    last_reported_minute = None
     print(f"Bot started. Running for {duration_minutes} minutes...")
+
     while datetime.now() < end_time:
         response = requests.get(API_URL + f"getUpdates?offset={offset}")
         updates = response.json().get('result', [])
@@ -52,10 +53,6 @@ def main_loop(duration_minutes=5):
                 role = check_user_db_role(user_id)
                 handler = CommandHandlerFactory().get_handler(role)
                 handler.handle(message)
-
-        # Виводимо залишок часу кожну хвилину
-
-        print(f"Time remaining: {(end_time - datetime.now()).seconds // 60} minutes")
 
     print("Bot finished running.")
 
